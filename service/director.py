409f0df09ee7d688d0211dd2f34d33d27f29672f
Nikolaev Valentin <val8673@gmail.com>
2022-11-12 16:29:13 +0600

    Add errorhandlers for directors

--- service/director.py
+++ service/director.py
@@ -5,8 +5,8 @@ class DirectorService:
     def __init__(self, dao: DirectorDAO):
         self.dao = dao
 
-    def get_one(self, bid):
-        return self.dao.get_one(bid)
+    def get_one(self, pk):
+        return self.dao.get_one(pk)
 
     def get_all(self):
         return self.dao.get_all()
